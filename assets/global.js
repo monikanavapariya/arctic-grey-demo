0695511f6ea5eecdd2e8c9c8cf8e862e5e952ade
monikamn <monika@stealth.design>
2025-02-24 19:58:20 +0530

    <!---- Real People Result ----> Create new section and add dynamic option and set design
    on click plus to item add in cart

--- assets/global.js
+++ assets/global.js
@@ -1294,6 +1294,79 @@ $(document).ready(function(){
     arrows: false,
     dots: false,
   });
+
+  var arrow_left = `<button class="slick-prev slick-arrow" aria-label="Previous" type="button"><svg xmlns="http://www.w3.org/2000/svg" width="12" height="10" viewBox="0 0 12 10" fill="none"><path d="M4.90179 0.182292L0.1875 4.55729C0.0625 4.67882 0 4.82639 0 5C0 5.17361 0.0625 5.32118 0.1875 5.44271L4.90179 9.81771C5.22321 10.0608 5.52679 10.0608 5.8125 9.81771C6.0625 9.50521 6.0625 9.21007 5.8125 8.93229L2.25 5.625H11.3571C11.75 5.59028 11.9643 5.38194 12 5C11.9643 4.61806 11.75 4.40972 11.3571 4.375H2.25L5.8125 1.06771C6.0625 0.789931 6.0625 0.494792 5.8125 0.182292C5.52679 -0.0607639 5.22321 -0.0607639 4.90179 0.182292Z" fill="#1B1F23"/></svg></button>`;
+    var arrow_right = `<button class="slick-next slick-arrow" aria-label="Next" type="button"><svg xmlns="http://www.w3.org/2000/svg" width="12" height="10" viewBox="0 0 12 10" fill="none"><path d="M7.09821 0.182292L11.8125 4.55729C11.9375 4.67882 12 4.82639 12 5C12 5.17361 11.9375 5.32118 11.8125 5.44271L7.09821 9.81771C6.77679 10.0608 6.47321 10.0608 6.1875 9.81771C5.9375 9.50521 5.9375 9.21007 6.1875 8.93229L9.75 5.625H0.642857C0.25 5.59028 0.0357143 5.38194 0 5C0.0357143 4.61806 0.25 4.40972 0.642857 4.375H9.75L6.1875 1.06771C5.9375 0.789931 5.9375 0.494792 6.1875 0.182292C6.47321 -0.0607639 6.77679 -0.0607639 7.09821 0.182292Z" fill="#1B1F23"/></svg></button>`;
+   
+
+  $('.real-people-result-slider').slick({
+    slidesToShow: 5,  // Make sure the middle slide is visible
+    slidesToScroll: 1,
+    infinite: true,
+    centerMode: true,  // Enable center mode
+    arrows: true,
+    dots: false,
+    prevArrow:arrow_left,
+    nextArrow:arrow_right,
+    centerPadding: '30px',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          centerPadding: '30px',
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          centerPadding: '30px',
+        }
+      }
+    ]
+  });
+
+  $('.real-people-result-slider li').removeClass('active-slide'); // Remove from all slides
+    $('.slick-center').addClass('active-slide'); // Add to the center slide
+
+  // Add 'active' class to the center slide
+  $('.real-people-result-slider').on('afterChange', function(event, slick, currentSlide){
+    $('.real-people-result-slider li').removeClass('active-slide'); // Remove from all slides
+    $('.slick-center').addClass('active-slide'); // Add to the center slide
+  });
+
+  // Trigger the class on page load
+  $('.real-people-result-slider').on('init', function(event, slick){
+    $('.slick-center').addClass('active-slide');
+  });
+
+  $(".real-people-result-btn a").on("click", function (e) {
+    e.preventDefault();
+    var var_id = $(this).attr('data-id')
+    $.ajax({
+      type: "POST",
+      url: "/cart/add.js",
+      data: {
+        id: var_id, // Replace with your actual product variant ID
+        quantity: 1
+      },
+      dataType: "json",
+      success: function (cartItem) {
+        
+        let updatedQuantity = cartItem.item_count || 0;
+$('.header__count-bubble span').text(updatedQuantity);
+$('cart-drawer').load(window.location.href + ' #CartDrawer');
+$('cart-drawer').removeClass('is-empty');
+$('cart-drawer').addClass('active');
+      },
+      error: function (error) {
+        console.log("Error adding to cart:", error);
+      }
+    });
+  });
+
+
 })
 
 
